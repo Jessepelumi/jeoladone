1540bf0403a784e3e8f6726afcfa0076eb44f1ae
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-21 14:42:38 +0100

    feat: modify contact message

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,7 +9,9 @@ const Contact = () => {
       <Title sn={"04"} title={"Contact"} />
       <h2>Get in Touch</h2>
       <p>
-        I'm always looking to expand my network and explore new opportunities for collaboration. You can send me an email or reach out to me on LinkedIn or X.
+        I'm always open to connecting with fellow engineers and exploring new
+        opportunities for collaboration. You can send me an email or reach out
+        to me via LinkedIn.
       </p>
       <span className="btn">
         <a href="mailto: jesseoluwapelumi01@gmail.com">
