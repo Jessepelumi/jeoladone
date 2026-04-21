24d0c1dc31969c140f206fbe4413fa02e8c97928
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-21 14:15:41 +0100

    feat: add new project to the list of projects

--- src/content/projectlist.js
+++ src/content/projectlist.js
@@ -20,10 +20,6 @@ export const projectlist = [
         icon: <GithubLogo size={24} />,
         link: "https://github.com/Jessepelumi/zimna",
       },
-      {
-        icon: <ArrowSquareOut size={24} />,
-        link: "https://github.com/Jessepelumi/zimna",
-      },
     ],
   },
   {
@@ -38,39 +34,10 @@ export const projectlist = [
         icon: <GithubLogo size={24} />,
         link: "https://github.com/Jessepelumi/segmentor",
       },
-      {
-        icon: <ArrowSquareOut size={24} />,
-        link: "https://github.com/Jessepelumi/segmentor",
-      },
     ],
   },
   {
     id: 3,
-    image: "kavodengineering.png",
-    project_title: "Zimna AI",
-    description:
-      "A professional website for KAVOD — a leading expert in materials quality assurance and quality control inspection. Built to streamline client engagement, the website features a dynamic service inquiry form, responsive design, and optimized user experience for prospective clients across various industries",
-    tools: [
-      "Python",
-      "TypeScript",
-      "Django",
-      "Next.js",
-      "PostgreSQL",
-      "Docker",
-    ],
-    links: [
-      {
-        icon: <GithubLogo size={24} />,
-        link: "https://github.com/Jessepelumi/kavodengineering",
-      },
-      {
-        icon: <ArrowSquareOut size={24} />,
-        link: "https://www.kavodengineering.com",
-      },
-    ],
-  },
-  {
-    id: 4,
     image: "eclipsered.png",
     project_title: "EclipseRed",
     description:
@@ -87,151 +54,54 @@ export const projectlist = [
       },
     ],
   },
-  // {
-  //   id: 2,
-  //   image: "belleville-project.png",
-  //   project_title: "Belleville Dentals",
-  //   description:
-  //     "Website aimed at bringing simplicity to accessible dental services and oral health education. It also features a dental products sales page",
-  //   tools: ["HTML", "CSS", "Bootstrap", "JavaScript", "JSON"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/BellevilleDental",
-  //     },
-  //     {
-  //       icon: <ArrowSquareOut size={24} />,
-  //       link: "https://jessepelumi.github.io/BellevilleDental/belleville.html#",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 3,
-  //   image: "EmployeeRecords.png",
-  //   project_title: "Employee Record Manager",
-  //   description:
-  //     "Employee records manager to properly store necessary info about employees, with functionalities that help search by selected filters, modify employee data and also delete employee records",
-  //   tools: ["Java"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/EmployeeRecordManager",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 4,
-  //   image: "styvic-project.png",
-  //   project_title: "Styvic Images",
-  //   description:
-  //     "One page website for Styvic Images, a media agency that combines creativity, technology, and innovation to redefine the way media is produced, managed, and distributed",
-  //   tools: ["HTML", "TailwindCSS"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/styvicImages",
-  //     },
-  //     {
-  //       icon: <ArrowSquareOut size={24} />,
-  //       link: "https://jessepelumi.github.io/styvicImages/public/styvic.html",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 5,
-  //   image: "",
-  //   project_title: "JEOLAD Portfolio V1",
-  //   description:
-  //     "This the first version of my personal website. My portfolio website is a curated showcase of my creative endeavors, projects, experiences, and journeys so far. ",
-  //   tools: ["React", "Styled Components"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/jeolad-portfolio-v1",
-  //     },
-  //     {
-  //       icon: <ArrowSquareOut size={24} />,
-  //       link: "https://jeoladone.netlify.app",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 6,
-  //   image: "",
-  //   project_title: "InstaVoid",
-  //   description:
-  //     "This is media-sharing, cross-platform mobile application. It leverages the functionalities of Flutter and FireBase to enable users upload and store media files. Users can also delete, like, and comment on posts.",
-  //   tools: ["Dart", "Flutter", "Riverpod", "FireBase"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/instavoid",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 7,
-  //   image: "",
-  //   project_title: "Ableton",
-  //   description:
-  //     "This is an 'ableton.com' clone. Ableton is a German music software company that produces and distributes unique software and hardware for music creation and performance.",
-  //   tools: ["React", "Sass"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/ableton",
-  //     },
-  //     { icon: <ArrowSquareOut size={24} />, link: "https://google.com" },
-  //   ],
-  // },
-  // {
-  //   id: 8,
-  //   image: "",
-  //   project_title: "SchoolManagementSystem",
-  //   description:
-  //     "The School Manager is a console-based system built with Python to manage teacher, student and course information, as well as take attendance and manage student results. The system features functionalities to create instanceses of, update, view, search and delete teachers, students and courses.",
-  //   tools: ["Python"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/SchoolManagementSystem",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 9,
-  //   image: "",
-  //   project_title: "Age Calculator",
-  //   description:
-  //     "Web calculator to calculate age. The purpose of this project was to put to practice form actions and error states in JavaScript.",
-  //   tools: ["HTMl", "CSS", "JavaScript"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/ageCalculator",
-  //     },
-  //     {
-  //       icon: <ArrowSquareOut size={24} />,
-  //       link: "https://jessepelumi.github.io/ageCalculator/public/ageCalculator.html",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 10,
-  //   image: "",
-  //   project_title: "Newsletter Signup",
-  //   description:
-  //     "Web page to subscriber to a newsletter, with a confirmation page. This project was to learn how to use email regex, form actions and error states in JavaScript.",
-  //   tools: ["HTMl", "CSS", "JavaScript"],
-  //   links: [
-  //     {
-  //       icon: <GithubLogo size={24} />,
-  //       link: "https://github.com/Jessepelumi/newsletter-signup",
-  //     },
-  //     {
-  //       icon: <ArrowSquareOut size={24} />,
-  //       link: "https://jessepelumi.github.io/newsletter-signup/public/newsletter.html",
-  //     },
-  //   ],
-  // },
+  {
+    id: 4,
+    image: "",
+    project_title: "FundCare",
+    description:
+      "FundCare is a health fundraising dashboard designed to help users track, manage, and visualize health-related fundraising campaigns in a clear and intuitive way. It provides a structured interface for monitoring donations, campaign progress, and key health metrics in real time.",
+    tools: ["JSON", "YAML"],
+    links: [
+      {
+        icon: <GithubLogo size={24} />,
+        link: "https://github.com/Jessepelumi/eclipseRed",
+      },
+      {
+        icon: <ArrowSquareOut size={24} />,
+        link: "https://marketplace.visualstudio.com/items?itemName=AdesinaJesse.eclipseRed",
+      },
+    ],
+  },
+  {
+    id: 5,
+    image: "kavodengineering.png",
+    project_title: "Kavod Engineering",
+    description:
+      "A professional website for KAVOD — a leading expert in materials quality assurance and quality control inspection. Built to streamline client engagement, the website features a dynamic service inquiry form, responsive design, and optimized user experience for prospective clients across various industries",
+    tools: ["TypeScript", "Next.js", "Tailwind", "AppsScript"],
+    links: [
+      {
+        icon: <GithubLogo size={24} />,
+        link: "https://github.com/Jessepelumi/kavodengineering",
+      },
+      {
+        icon: <ArrowSquareOut size={24} />,
+        link: "https://www.kavodengineering.com",
+      },
+    ],
+  },
+  {
+    id: 6,
+    image: "EmployeeRecords.png",
+    project_title: "Employee Record Manager",
+    description:
+      "Employee records manager to properly store necessary info about employees, with functionalities that help search by selected filters, modify employee data and also delete employee records",
+    tools: ["Java"],
+    links: [
+      {
+        icon: <GithubLogo size={24} />,
+        link: "https://github.com/Jessepelumi/EmployeeRecordManager",
+      },
+    ],
+  },
 ];
